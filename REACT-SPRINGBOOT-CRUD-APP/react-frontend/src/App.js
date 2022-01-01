93de00f2406c05d3f1ffb3cc49f4c44bfbc41fe3
iterics-rishi <developer.iterics@gmail.com>
2022-01-01 14:59:36 -0500

    commit-added the list employee component

--- REACT-SPRINGBOOT-CRUD-APP/react-frontend/src/App.js
+++ REACT-SPRINGBOOT-CRUD-APP/react-frontend/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import FooterComponent from "./components/FooterComponent";
+import HeaderComponent from "./components/HeaderComponent";
+import ListEmployeeComponent from "./components/ListEmployeeComponent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HeaderComponent />
+      <div className="container">
+        <ListEmployeeComponent />
+      </div>
+      <FooterComponent />
     </div>
   );
 }
--- springboot-backend/src/main/java/com/rishi/springboot/controller/EmployeeController.java
+++ springboot-backend/src/main/java/com/rishi/springboot/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.rishi.springboot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.rishi.springboot.model.Employee;
 import com.rishi.springboot.repository.EmployeeRepository;
 
+
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
