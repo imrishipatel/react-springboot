0600ce1dbc306b3647e8435b6de0656fda5701df
iterics-rishi <developer.iterics@gmail.com>
2022-01-01 17:53:34 -0500

    update Emploiyee REST API

--- springboot-backend/src/main/java/com/rishi/springboot/controller/EmployeeController.java
+++ springboot-backend/src/main/java/com/rishi/springboot/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,11 +39,24 @@ public class EmployeeController {
 		return employeeRepository.save(employee); 
 	}
 	
-	//get employee by is REST API 
+	//get employee by id REST API 
 	@GetMapping("/employees/{id}")
 	public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
 		Employee employee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id)); 
 		return ResponseEntity.ok(employee); 
+	}
+	
+	//update employee REST API 
+	@PutMapping("/employees/{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails){
+		Employee employee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id)); 
+		employee.setFirstName(employeeDetails.getFirstName()); 
+		employee.setLastName(employeeDetails.getLastName());
+		employee.setEmailId(employeeDetails.getEmailId()); 
+		
+		Employee updatedEmployee = employeeRepository.save(employee); 
+		
+		return ResponseEntity.ok(updatedEmployee); 
 	}
 	
 }
